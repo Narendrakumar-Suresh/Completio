0f4b4f177fdb7c5c5c5828af9c008115b44db344
Narendrakumar Suresh <138976636+Narendrakumar-Suresh@users.noreply.github.com>
2023-09-22 10:17:33 +0000

    Updation in  ui and functionalities

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
 var ongoing=[];
 var completed=[];
+const checkboxContainer = document.querySelector('#checkbox-container');
 
 function tasker(){
     var task=document.getElementById("tasks");
     ongoing.push(task.value);
     // alert(tasky);
     task.value='';
+    displayTasksAsCheckboxes();
+    //contentview(ongoing);
 }
+
+function displayTasksAsCheckboxes() {
+    var ccontent = document.getElementById("content");
+    ccontent.innerHTML = ''; // Clear the existing content
+
+    for (var i = 0; i < ongoing.length; i++) {
+        var checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.value = ongoing[i];
+        checkbox.id = "task-" + i;
+
+        var label = document.createElement("label");
+        label.htmlFor = "task-" + i;
+        label.appendChild(document.createTextNode(ongoing[i]));
+
+        var div = document.createElement("div");
+        div.appendChild(checkbox);
+        div.appendChild(label);
+
+        ccontent.appendChild(div);
+    }
+}
+
+
+
+
+
 
