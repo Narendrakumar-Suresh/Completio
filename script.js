cea3e3d682b5157d08d255d3436b7b3e8c0d3307
Narendrakumar Suresh <138976636+Narendrakumar-Suresh@users.noreply.github.com>
2023-10-18 11:11:53 +0000

    Beta 2

--- script.js
+++ script.js
@@ -1,45 +1,39 @@
-var ongoing=[];
-var completed=[];
-var checkbox = document.createElement("input");
-var label = document.createElement("label");
-const checkboxContainer = document.querySelector('#checkbox-container');
-var element;
+var ongoing = [];
+var completed = [];
+var checkboxContainer = document.querySelector('#checkbox-container');
 
-function tasker(){
-    var task=document.getElementById("tasks");
-    let t=task.value;
-    if(t.trim()===""){
-        alert("Enter some value.");
-    }else{
-        ongoing.push(t);
-        contentview();
-        task.value='';
-    }
-    // alert(tasky);
-    
+function tasker() {
+  var task = document.getElementById("tasks");
+  let t = task.value;
+  if (t.trim() === "") {
+    alert("Enter some value.");
+  } else {
+    ongoing.push(t);
+    contentview();
+    task.value = '';
+  }
 }
 
 function contentview() {
-    var ccontent = document.getElementById("content");
-    ccontent.innerHTML = ''; // Clear the existing content
+  var ccontent = document.getElementById("content");
+  // Clear the existing content
+  ccontent.innerHTML = '';
 
-    for (var i = 0; i < ongoing.length; i++) {
-        checkbox.type = "checkbox";
-        checkbox.value = ongoing[i];
-        checkbox.id = "task-" + i;
+  for (var i = 0; i < ongoing.length; i++) {
+    var checkbox = document.createElement("input");
+    var label = document.createElement("label");
+    var div = document.createElement("div");
 
-        label.htmlFor = "task-" + i;
-        label.appendChild(document.createTextNode(ongoing[i]));
+    checkbox.type = "checkbox";
+    checkbox.value = ongoing[i];
+    checkbox.id = "task-" + i;
 
-        var div = document.createElement("div");
-        div.appendChild(checkbox);
-        div.appendChild(label);
+    label.htmlFor = "task-" + i;
+    label.appendChild(document.createTextNode(ongoing[i]));
 
-        ccontent.appendChild(div);
-    }
+    div.appendChild(checkbox);
+    div.appendChild(label);
+
+    ccontent.appendChild(div);
+  }
 }
-
-
-
-
-
